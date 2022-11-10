cb9257bd42fb63bda2fb768b9259eedfe4cb92cc
Kamronnahar-Monisha <kamronnaharmonisha12@gmail.com>
2022-11-10 16:32:05 +0600

    error massage added

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -4,17 +4,19 @@ import discountImg from '../../../images/discount.svg';
 import { AuthContext } from '../../../Context/AuthProvider';
 
 const Contact = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     return (
-        <div className='row justify-content-center align-items-center my-5'>
-            <div className="col-lg-4">
-                <img src={discountImg} className="w-100" alt="" />
-            </div>
-            <div className="col-lg-4">
-                <h2 className='theme-color-turquoise'>Get Discount Information</h2>
-                <input className='form-control text-muted contact-email' type="text" defaultValue={user?user.email : ""} />
-                <br />
-                <button className='contact-btn rounded fs-6'>Contact Us</button>
+        <div className="container">
+            <div className='row justify-content-center align-items-center my-5'>
+                <div className="col-lg-4">
+                    <img src={discountImg} className="w-100" alt="" />
+                </div>
+                <div className="col-lg-4">
+                    <h2 className='theme-color-turquoise'>Get Discount Information</h2>
+                    <input className='form-control text-muted contact-email' type="text" defaultValue={user ? user.email : ""} />
+                    <br />
+                    <button className='contact-btn rounded fs-6'>Contact Us</button>
+                </div>
             </div>
         </div>
     );
